4738c96878a81c8bd995a90250dc04c00465c82e
Victor Deon <victorhad@gmail.com>
2023-05-14 18:28:46 -0300

    Finish use state

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseState = (props) => {
+// Recebe um estado em um componente funcional
+// Componentes controlados são componentes vinculados a um estado da aplicação, exemplo, se o input
+// está vinculado a um useState ele é um componente controlado.
+const UseState = props => {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercício #01" />
+
+            <div className='center'>
+                <span className='text'>{count}</span>
+                <div>
+                    <button className='btn' onClick={() => setCount(count - 1)}>-1</button>
+                    <button className='btn' onClick={() => setCount(current => current + 1)}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+
+            <input
+                type='text'
+                className='input'
+                value={name}
+                onChange={event => setName(event.target.value)}
+            />
+            <span className='text'>Texto: {name}</span>
         </div>
     )
 }
