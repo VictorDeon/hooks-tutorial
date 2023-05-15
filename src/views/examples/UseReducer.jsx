422170be20d872f16570c6f17479dc133f38a1e5
Victor Deon <victorhad@gmail.com>
2023-05-14 21:01:28 -0300

    useReducer tutorial

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseReducer = (props) => {
+const initialState = {
+    number: 0,
+    text: '...'
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'addNumber':
+            return {
+                ...state,
+                number: state.number + action.number
+            }
+        default:
+            return state
+    }
+}
+
+const UseReducer = props => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+
+            <div className='center'>
+                <span className='text'>{state.number}</span>
+                <div>
+                    <button className='btn' onClick={() => dispatch({type: "addNumber", number: 2})}>+2</button>
+                </div>
+            </div>
         </div>
     )
 }
